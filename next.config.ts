fe36e4aff72bceee355fe5abecd6c9dc5a24711a
russelhoran <russelfernandes35@gmail.com>
2025-06-25 12:49:14 +0530

    export changes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true, 
-  },
-  basePath: '/portfoliowebsite', 
-  assetPrefix: '/portfoliowebsite',
-};
-
-module.exports = nextConfig;
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
